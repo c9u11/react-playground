3ab09208a97b0ce6fe8e91bc8d426a0f20972b9e
c9u11 <tjrdud6412@naver.com>
2021-11-24 21:59:22 +0900

    add select coins /w jsx, object.map()

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : 
+        <select>
+          {coins.map(coin => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      }
     </div>
   );
 }
