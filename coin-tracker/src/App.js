185cee573b55de1d37cc74c11cd33e0caf7bdb40
c9u11 <tjrdud6412@naver.com>
2021-11-24 21:50:06 +0900

    get data /w useState, useEffect

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }, []);
   return (
     <div>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
     </div>
   );
 }
