18edc4a797189824cd3b9a39c67f339ae4679319
c9u11 <tjrdud6412@naver.com>
2021-12-07 23:11:45 +0900

    use Typescript > use state, event

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Circle from "./Circle";
+import { eventNames } from "process";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  }
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black"></Circle>
-      <Circle bgColor="tomato" text="Tomato"></Circle>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username"></input>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
