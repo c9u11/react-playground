7116763f2d614d887978b6098a08639825a6b5bc
c9u11 <tjrdud6412@naver.com>
2021-12-14 21:44:07 +0900

    todo list project init setting

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2B303B",
+  borderColor: "#58627d",
+  primaryTextColor: "#FFFFFF",
+  secondaryTextColor: "#C4C9D4",
+  errorColor: "#B42818",
+  accentColor: "#FF9500",
+  boxBgColor: "#555c6e",
+  boxTextColor: "#f1f2f4"
+}
