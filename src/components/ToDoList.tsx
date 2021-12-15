af600a1496fa53dcbe46888e2bb139f0c568912b
c9u11 <tjrdud6412@naver.com>
2021-12-15 22:44:18 +0900

    todoList 카테고리 별로 보여주기 /w selector

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,17 +1,30 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo></CreateToDo>
+      <h2>ToDo</h2>
       <ul>
         {toDos.map(toDo => {
+          return (<ToDo {...toDo} key={toDo.id}></ToDo>);
+        })}
+      </ul>
+      <h2>Doing</h2>
+      <ul>
+        {doing.map(toDo => {
+          return (<ToDo {...toDo} key={toDo.id}></ToDo>);
+        })}
+      </ul>
+      <h2>Done</h2>
+      <ul>
+        {done.map(toDo => {
           return (<ToDo {...toDo} key={toDo.id}></ToDo>);
         })}
       </ul>
