85f45019c0c2ccccfe738aecbba5c2551b2af8b4
c9u11 <tjrdud6412@naver.com>
2021-12-18 01:28:04 +0900

    Example3 /w whileHover, whileTap

--- README.md
+++ README.md
@@ -225,6 +225,25 @@ function App() {
 }
 ```
 
+- Example3
+
+![Example3](https://user-images.githubusercontent.com/29428714/146576414-64a9ad76-ccc1-43e8-a583-99c24c9ca5a3.gif)
+
+```tsx
+const boxVariants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { borderRadius: "50%", scale: 1 }
+}
+function App() {
+  return (
+    <Wrapper>
+      <Box variants={boxVariants} whileHover="hover" whileTap="click">
+      </Box>
+    </Wrapper>
+  );
+}
+```
+
 
 
 ------
--- src/App.tsx
+++ src/App.tsx
@@ -10,60 +10,20 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: rgba(255,255,255,0.2);
+  background-color: rgba(255,255,255,1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const Circle = styled(motion.div)`
-  place-self: center;
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.3,
-      staggerChildren: 0.1
-    }
-  }
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { borderRadius: "50%", scale: 1 }
 }
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10
-  },
-  end: {
-    opacity: 1,
-    y: 0
-  }
-}
-
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
+      <Box variants={boxVariants} whileHover="hover" whileTap="click">
       </Box>
     </Wrapper>
   );
